51b607f6150b449652a1551b2fc4723abdb8f090
Blazej Przyluski <blazejprzyl@gmail.com>
2019-03-26 22:52:19 +0100

    Change in gui, more readable and easier to implement

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -7,40 +7,17 @@ import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
-    @FXML
-    private TableView<Day> daysTable;
 
     @FXML
-    private TableView<Tuesday> tuesdayTable;
+    private GridPane mainGridPane;
 
-    @FXML
-    private TableColumn testColumn;
 
-    @FXML
-    private TableColumn mondayColumn;
-
-    @FXML
-    private TableColumn tuesdayColumn;
-
-    @FXML
-    private TableColumn wendesdayColumn;
-
-    @FXML
-    private TableColumn thursdayColumn;
-
-    @FXML
-    private TableColumn fridayColumn;
-
-    @FXML
-    private TableColumn saturdayColumn;
-
-    @FXML
-    private TableColumn sundayColumn;
 
     private List<Day> daysOfTheWeek = new ArrayList<>();
 
@@ -57,62 +34,14 @@ public class Controller {
 
         ObservableList<Day> daysList = FXCollections.observableArrayList(monday,tuesday,wednesday,thursday,friday,saturday,sunday);
         //test tableview
-        ObservableList<Tuesday> tuesdays = FXCollections.observableArrayList(tuesday);
 
 
 
-        mondayColumn.setCellValueFactory(
-                new PropertyValueFactory<Monday,String>("workoutData")
-        );
-
-        tuesdayColumn.setCellValueFactory(
-                new PropertyValueFactory<Tuesday,String>("workoutData")
-        );
-
-        wendesdayColumn.setCellValueFactory(
-                new PropertyValueFactory<Wednesday,String>("workoutData")
-        );
-
-        thursdayColumn.setCellValueFactory(
-                new PropertyValueFactory<Thursday,String>("workoutData")
-        );
-
-        fridayColumn.setCellValueFactory(
-                new PropertyValueFactory<Friday,String>("workoutData")
-        );
-
-        saturdayColumn.setCellValueFactory(
-                new PropertyValueFactory<Saturday,String>("workoutData")
-        );
-
-        sundayColumn.setCellValueFactory(
-                new PropertyValueFactory<Sunday,String>("workoutData")
-        );
-
-
-        testColumn.setCellValueFactory(
-                new PropertyValueFactory<Tuesday,String>("workoutData")
-        );
-
-
-        //test
-        tuesday.setWorkoutData("chuj");
-        wednesday.setWorkoutData("cipa");
-        thursday.setWorkoutData("anal");
-
-        daysTable.setItems(daysList);
-        tuesdayTable.setItems(tuesdays);
-
-        //Allows selection of just one cell
-        daysTable.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        daysTable.getSelectionModel().setCellSelectionEnabled(true);
     }
 
     @FXML
-    public void addWorkoutToDay()
+    public void addWorkoutToMonday()
     {
-
-        System.out.println(daysTable.getSelectionModel().getSelectedItem().getDate().toString());
 
     }
 
